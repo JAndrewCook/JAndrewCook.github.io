f8bfae4182438ade86f5674c7304dd9ad0468af9
JAndrewCook <jandrewc@protonmail.com>
2017-01-26 09:22:38 -0700

    some changes

--- index.php
+++ index.php
@@ -1,17 +1,36 @@
-<?php
+<!-- <?php
 
 $page_title = 'Home | John Cook';
 $page_fun = 'A little about me...';
 include ('includes/header.html');
 
-?>
-<main>
-  <h1>John Cook</h1>
-<br/>
-  <p>A portfolio site to hold all of my coding projects and examples.</p>
-</main>
-
-<?php
+?> -->
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title><?php echo $page_title; ?></title>
+  <link rel="stylesheet" href="includes/port.css" type="text/css">
+</head>
+<body>
+  <header>
+    <h1>John C</h1>
+    <h2><?php echo $page_fun; ?></h2>
+  </header>
+    <main>
+      <h1>John Cook</h1>
+      <br/>
+      <p>A portfolio site to hold all of my coding projects and examples.</p>
+    </main>
+  <footer>
+    <p>Copyright &copy; John Cook 2016 | Designed by John Cook |
+      Sponsored by <a href="http://www.mtchs.org/">MTCHS</a> | Valid
+      <a href="#">CSS</a> &amp;
+      <a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Fjohnc.smtchs.org%2FJohnAMcCCook%2Fwebsite%2F">XHTML</a></p>
+  </footer>
+</body>
+</html>
+<!-- <?php
 
 include ('includes/footer.html');
-?>
+?> -->
